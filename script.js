b50184328b52b30e2e0a9bf6da4b3d81a0b93445
Joel Alberto Pichardo Arayon <joelalbertopichardoalayon@gmail.com>
2025-05-23 15:17:47 -0400

    genres and new ssscons

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+-document.addEventListener("DOMContentLoaded", function () {
     const loader = document.querySelector(".loader");
     const before_loader = document.querySelector("before_loader");
 
